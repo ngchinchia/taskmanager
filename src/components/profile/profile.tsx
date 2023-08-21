d9b5c626099a04f434c4ff6dbce5ace99c50c357
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-21 19:55:44 +0800

    created profile

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,44 @@
+import React, { FC, ReactElement } from "react";
+import { Avatar, Box, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+interface IProfile {
+  name: string;
+}
+//Function component accpets generic type of IProfile
+export const Profile: FC<IProfile> = (props): ReactElement => {
+  // Destructure props
+  const { name = "John" } = props;
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Avatar
+        sx={{
+          width: "96px",
+          height: "96px",
+          backgroundColor: "primary.main",
+          marginBottom: "16px",
+        }}
+      >
+        <Typography variant="h4" color="text.primary">
+          J
+        </Typography>
+      </Avatar>
+
+      <Typography variant="h6" color="text.primary">
+        {`Welcome, ${name}`}
+      </Typography>
+      <Typography variant="body1" color="text.primary">
+        This is your personal task manager
+      </Typography>
+    </Box>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+};
