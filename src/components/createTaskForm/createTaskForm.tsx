fe136021b9a9b9d74687bc1514b9e4aa6abe00d2
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-22 19:48:34 +0800

    complete taskform

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -4,6 +4,11 @@ import { Box, Typography, Stack } from "@mui/material";
 import { TaskTitleField } from "./_taskTitleField";
 import { TaskDescriptionField } from "./_taskDescriptionField";
 import { TaskDateField } from "./_taskDateField";
+import { TaskSelectField } from "./_taskSelectField";
+
+import { Status } from "./enums/Status";
+import { Priority } from "./enums/Priority";
+
 /* -Task Title
    -Task Description
    -Task Date 
@@ -27,6 +32,41 @@ export const CreateTaskForm: FC = (): ReactElement => {
         <TaskTitleField />
         <TaskDescriptionField />
         <TaskDateField />
+
+        <Stack sx={{ width: "100%" }} direction="row" spacing={2}>
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              {
+                value: Status.todo,
+                label: Status.todo.toUpperCase(),
+              },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+            ]}
+          />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            items={[
+              {
+                value: Priority.low,
+                label: Priority.low,
+              },
+              {
+                value: Priority.normal,
+                label: Priority.normal,
+              },
+              {
+                value: Priority.high,
+                label: Priority.high,
+              },
+            ]}
+          />
+        </Stack>
       </Stack>
     </Box>
   );
