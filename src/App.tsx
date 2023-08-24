d21accf7d16f7cf94c80503fbd84a9c10db47890
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-24 15:04:35 +0800

    add sendApiRequest

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, { FC, ReactElement } from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { customTheme } from "./theme/customTheme";
 import { Dashboard } from "./pages/dashboard/dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 /* Calls app as type functional component and return a jsx constructor */
+
+// Create a client
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline/>
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
