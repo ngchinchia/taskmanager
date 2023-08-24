222d9baf0614cb4ae1ef3caa014e748dfbbb9f4c
Ng Chin Chia <billyngchinchia@gmail.com>
2023-08-24 17:11:34 +0800

    add count func

--- src/components/task/_taskFooter.tsx
+++ src/components/task/_taskFooter.tsx
@@ -2,9 +2,12 @@ import React, { FC, ReactElement } from "react";
 import { Box, Button, Switch, FormControlLabel } from "@mui/material";
 import { ITaskFooter } from "./interfaces/ITaskFooter";
 import PropTypes from "prop-types";
+import { Status } from "../createTaskForm/enums/Status";
 
 export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
   const {
+    id,
+    status,
     onStatusChange = (e) => console.log(e),
     onClick = (e) => console.log(e),
   } = props;
@@ -18,14 +21,20 @@ export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
     >
       <FormControlLabel
         label="In Progress"
-        control={<Switch onChange={(e) => onStatusChange(e)} color="warning" />}
+        control={
+          <Switch
+            onChange={(e) => onStatusChange(e,id)}
+            color="warning"
+            defaultChecked={status === Status.inProgress}
+          />
+        }
       />
       <Button
         variant="contained"
         color="success"
         size="small"
         sx={{ color: "#ffffff" }}
-        onClick={(e) => onClick(e)}
+        onClick={(e) => onClick(e, id)}
       >
         Mark Complete
       </Button>
@@ -34,6 +43,8 @@ export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
 };
 
 TaskFooter.propTypes = {
-    onStatusChange: PropTypes.func,
-    onClick: PropTypes.func,
-}
+  id: PropTypes.string.isRequired,
+  status: PropTypes.string,
+  onStatusChange: PropTypes.func,
+  onClick: PropTypes.func,
+};
--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -21,6 +21,8 @@ export const Task: FC<ITask> = (props): ReactElement => {
     status = Status.completed,
     onStatusChange = (e) => console.log(e),
     onClick = (e) => console.log(e),
+    id,
+
   } = props;
   return (
     <Box
@@ -40,7 +42,7 @@ export const Task: FC<ITask> = (props): ReactElement => {
     >
       <TaskHeader title={title} date={date} />
       <TaskDescription description={description} />
-      <TaskFooter onClick={onClick} onStatusChange={onStatusChange} />
+      <TaskFooter id={id} status={status} onClick={onClick} onStatusChange={onStatusChange} />
     </Box>
   );
 };
